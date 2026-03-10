d6e7614f5f94e118b33c32d75a5d2bed92897420
Gianfranco Manca <90779383+GianfrancoMan@users.noreply.github.com>
2026-03-10 14:51:00 +0100

    Refactor ChatClientConfig by removing comments
    Removed Italian comments and cleaned up code formatting.

--- src/main/java/com/eazybytes/multimodel/config/ChatClientConfig.java
+++ src/main/java/com/eazybytes/multimodel/config/ChatClientConfig.java
@@ -6,7 +6,7 @@ import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/*creo e configuro i bean ChatClient per per i model openai e ollama...*/
+
 @Configuration
 public class ChatClientConfig {
 
@@ -17,12 +17,9 @@ public class ChatClientConfig {
 
     @Bean
     public ChatClient ollamaChatClient(OllamaChatModel ollamaChatModel) {
-        /*Il metodo create() usato sopra non fa altro che creare un ChatClient.Builder
-        * e invocare su di esso il metodo build() come segue...*/
+        
         ChatClient.Builder chatClientBuilder = ChatClient.builder(ollamaChatModel);
         return chatClientBuilder.build();
-        /*Con questo tipo di configurazione si ha maggiore controllo su quale deve essere il
-        * comportamento del bean ChatClient in vocando altri metodi sul ChatClientBuilder prima del build()...*/
     }
 
 }
